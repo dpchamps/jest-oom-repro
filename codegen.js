5577126fc1c47ff9d816e7dcb5f50eebad187248
David <david.campion@walmartlabs.com>
2022-05-05 15:37:44 -0700

    save

--- codegen.js
+++ codegen.js
@@ -11,13 +11,13 @@ Array(numSpecs)
   .fill(0)
   .forEach((_, i) => {
     const spec = `
-  describe("some-evil-spec=${i}", () => {
-    it("evil-block-${i}", () => {
-      window.evil_${i} = Array(1_000_000).fill(0).map((__, j) => j === 0 ? "spec-${i}" : Math.random())
+  describe("spec-${i}", () => {
+    it("block-${i}", () => {
+      window.memory_${i} = Array(1_000_000).fill(0).map((__, j) => j === 0 ? "spec-${i}" : Math.random())
       expect(0).toEqual(0)
     });
   });
   `;
-    const evilSpecFilename = path.resolve(specsDirname, `evil-${i}.spec.js`);
+    const evilSpecFilename = path.resolve(specsDirname, `${i}.spec.js`);
     fs.writeFileSync(evilSpecFilename, spec);
   });
---
+++ globalSetup.js
@@ -0,0 +1,42 @@
+const {createWriteStream} = require('fs');
+const vm = require('node:v8');
+module.exports = async () => {}
+const ws = createWriteStream(`${__dirname}/${process.version}-${String(Date.now()).substring(-5)}`);
+
+ws.write([
+    'Total Heap Size',
+    "Used Heap Size",
+    "Heap Size Limit",
+    "Number of Native Contexts",
+    "RSS",
+    "External",
+    "Array Buffers",
+    "Process Heap Used"
+].join(","));
+ws.write("\n");
+
+setInterval(() => {
+    const {
+        total_heap_size,
+        used_heap_size,
+        heap_size_limit,
+        number_of_native_contexts
+    } = vm.getHeapStatistics();
+    const {
+        rss,
+        external,
+        arrayBuffers,
+        heapUsed
+    } = process.memoryUsage();
+    ws.write([
+        total_heap_size,
+        used_heap_size,
+        heap_size_limit,
+        number_of_native_contexts,
+        rss,
+        external,
+        arrayBuffers,
+        heapUsed
+    ].join(","))
+    ws.write("\n");
+}, 500);
